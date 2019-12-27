4fc1923323543db849deccbf2998646cdf1bae31
Brad Moore <brad@4pi.com.au>
2019-12-27 12:43:21 +1100

    Added Pint + new XR unlock system.

---
+++ OWCE/OWCE.iOS/DependencyImplementations/UserAgent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using BigTed;
+using CoreGraphics;
+using Foundation;
+using OWCE.DependencyInterfaces;
+using WebKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(OWCE.iOS.DependencyImplementations.UserAgent))]
+
+namespace OWCE.iOS.DependencyImplementations
+{
+    public class UserAgent : IUserAgent
+    {
+        public Task<string> GetSystemUserAgent()
+        {
+            var taskCompletionSource= new TaskCompletionSource<string>();
+            var webView = new WKWebView(CGRect.Empty, new WKWebViewConfiguration());
+            webView.EvaluateJavaScript("navigator.userAgent", (NSObject result, NSError err) =>
+            {
+                if (result != null && result is NSString resultString)
+                {
+                    taskCompletionSource.TrySetResult(resultString.ToString());
+                    return;
+                }
+
+                // Return empty string if it failed.
+                taskCompletionSource.TrySetResult(String.Empty);
+            });
+            return taskCompletionSource.Task;
+        }
+    }
+}
