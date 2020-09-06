cca7974a6501c4db6f8b994a9cb7368107ba2888
Brad Moore <brad@4pi.com.au>
2020-09-06 12:26:51 +1000

    Implemented power view group.

--- OWCE/OWCE/Views/PowerView.xaml.cs
+++ OWCE/OWCE/Views/PowerView.xaml.cs
@@ -6,6 +6,50 @@ namespace OWCE.Views
 {
     public partial class PowerView : ContentView
     {
+        public static readonly BindableProperty CurrentAmpsProperty = BindableProperty.Create(
+            "CurrentAmps",
+            typeof(double),
+            typeof(PowerView));
+
+        public double CurrentAmps
+        {
+            get { return (double)GetValue(CurrentAmpsProperty); }
+            set
+            {
+                SetValue(CurrentAmpsProperty, value);
+            }
+        }
+
+
+        public static readonly BindableProperty TripAmpHoursProperty = BindableProperty.Create(
+            "TripAmpHours",
+            typeof(double),
+            typeof(PowerView));
+
+        public double TripAmpHours
+        {
+            get { return (double)GetValue(TripAmpHoursProperty); }
+            set
+            {
+                SetValue(TripAmpHoursProperty, value);
+            }
+        }
+
+
+        public static readonly BindableProperty TripRegenAmpHoursProperty = BindableProperty.Create(
+            "TripRegenAmpHours",
+            typeof(double),
+            typeof(PowerView));
+
+        public double TripRegenAmpHours
+        {
+            get { return (double)GetValue(TripRegenAmpHoursProperty); }
+            set
+            {
+                SetValue(TripRegenAmpHoursProperty, value);
+            }
+        }
+
         public PowerView()
         {
             InitializeComponent();
