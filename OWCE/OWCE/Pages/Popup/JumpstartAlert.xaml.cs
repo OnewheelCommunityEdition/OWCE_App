b79d9c6bc2eca388efef361e75a8536195804d07
Brad Moore <brad@4pi.com.au>
2021-02-14 10:19:03 +1100

    Didnt commit this file before.

---
+++ OWCE/OWCE/Pages/Popup/JumpstartAlert.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace OWCE.Pages.Popup
+{
+    public partial class JumpstartAlert : PopupPage
+    {
+        private readonly Command _actionButtonCommand;
+        public Command ActionButtonCommand => _actionButtonCommand;
+
+        public JumpstartAlert(Command actionCommand)
+        {
+            _actionButtonCommand = actionCommand;
+
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
