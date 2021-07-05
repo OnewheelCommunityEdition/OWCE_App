ae93c97311869cd3a7fb381c1dcf82fe3652b952
koa28 <86817367+koa28@users.noreply.github.com>
2021-07-04 23:18:19 -0700

    No-op watch dependency implementation for Android

---
+++ OWCE/OWCE.Android/DependencyImplementations/Watch.cs
@@ -0,0 +1,35 @@
+﻿using OWCE.DependencyInterfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(OWCE.Droid.DependencyImplementations.Watch))]
+
+namespace OWCE.Droid.DependencyImplementations
+{
+    public class Watch : IWatch
+    {
+        public void BoardConnected()
+        {
+            // Implement when we have Android Watch support
+        }
+
+        public void UpdateBatteryPercent(int percent)
+        {
+            // Implement when we have Android Watch support
+        }
+
+        public void UpdateDistance(string distanceString)
+        {
+            // Implement when we have Android Watch support
+        }
+
+        public void UpdateSpeed(int speed)
+        {
+            // Implement when we have Android Watch support
+        }
+
+        public void UpdateVoltage(float voltage)
+        {
+            // Implement when we have Android Watch support
+        }
+    }
+}
