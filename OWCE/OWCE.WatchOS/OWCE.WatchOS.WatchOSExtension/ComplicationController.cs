cd5dd8ee5a402aac3cfb660a3a2d006506ff86b8
koa28 <86817367+koa28@users.noreply.github.com>
2021-07-05 21:34:47 -0700

    Support complication for Infograph Modular (GraphicalCircular) and show the logo properly.

--- OWCE/OWCE.WatchOS/OWCE.WatchOS.WatchOSExtension/ComplicationController.cs
+++ OWCE/OWCE.WatchOS/OWCE.WatchOS.WatchOSExtension/ComplicationController.cs
@@ -15,76 +15,72 @@ namespace OWCE.WatchOS.WatchOSExtension
 
 		public override void GetComplicationDescriptors(Action<CLKComplicationDescriptor[]> handler)
         {
-			CLKComplicationFamily[] family = { CLKComplicationFamily.ModularSmall };
+            // For now, only support Modular and GraphicCircular (Modular Infograph)
+			CLKComplicationFamily[] family = {
+                CLKComplicationFamily.ModularSmall,
+                CLKComplicationFamily.GraphicCircular };
 			CLKComplicationDescriptor desc = new CLKComplicationDescriptor("OWCE", "OWCE", family);
 			CLKComplicationDescriptor[] descriptors = { desc };
 			handler(descriptors);
 		}
 
-		public override void GetCurrentTimelineEntry(CLKComplication complication, Action<CLKComplicationTimelineEntry> handler)
-		{
-			// Call the handler with the current timeline entry
-			Console.WriteLine("GetCurrentTimelineEntry");
+        public override void GetCurrentTimelineEntry(CLKComplication complication, Action<CLKComplicationTimelineEntry> handler)
+        {
+            // Call the handler with the current timeline entry
+            Console.WriteLine("GetCurrentTimelineEntry");
 
-			CLKComplicationTimelineEntry entry = null;
-			try
-			{
-				var minutesPastHour = DateTime.Now.Minute.ToString();
+            CLKComplicationTimelineEntry entry = null;
+            try
+            {
+                var minutesPastHour = DateTime.Now.Minute.ToString();
 
-				if (complication.Family == CLKComplicationFamily.ModularSmall)
-				{
-					var textTemplate1 = new CLKComplicationTemplateModularSmallSimpleText();
-					textTemplate1.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
-					entry = CLKComplicationTimelineEntry.Create(NSDate.Now, textTemplate1);
-					//var imageTemplate1 = new CLKComplicationTemplateModularSmallSimpleImage();
-					//var imageProvider = new CLKImageProvider();
-					//imageProvider.OnePieceImage = UIImage.FromBundle();
-					//imageTemplate1.ImageProvider = imageProvider;
+                if (complication.Family == CLKComplicationFamily.ModularSmall)
+                {
+                    var textTemplate1 = new CLKComplicationTemplateModularSmallSimpleText();
+                    textTemplate1.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
+                    entry = CLKComplicationTimelineEntry.Create(NSDate.Now, textTemplate1);
+                }
+                else if (complication.Family == CLKComplicationFamily.GraphicCircular)
+                {
+                    var graphicTemplate = new CLKComplicationTemplateGraphicCircularImage();
+                    graphicTemplate.ImageProvider = new CLKFullColorImageProvider(
+                        UIImage.FromBundle("OWCELogo"));
 
-				}
+                    entry = CLKComplicationTimelineEntry.Create(NSDate.Now, graphicTemplate);
+                }
 
-			}
-			catch (Exception x)
-			{
-				Console.WriteLine("Exception " + x);
-			}
-			handler(entry);
-		}
-		public override void GetPlaceholderTemplate(CLKComplication complication, Action<CLKComplicationTemplate> handler)
-		{
-			// This method will be called once per supported complication, and the results will be cached
-			Console.WriteLine("GetPlaceholderTemplate for " + complication);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Exception " + x);
+            }
+            handler(entry);
+        }
 
-			CLKComplicationTemplate template = null;
+        public override void GetPlaceholderTemplate(CLKComplication complication, Action<CLKComplicationTemplate> handler)
+        {
+            // This method will be called once per supported complication, and the results will be cached
+            Console.WriteLine("GetPlaceholderTemplate for " + complication);
 
-			if (complication.Family == CLKComplicationFamily.ModularSmall)
-			{
-				var textTemplate = new CLKComplicationTemplateModularSmallSimpleText();
-				textTemplate.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
-				template = textTemplate;
-			}
-			//else if (complication.Family == CLKComplicationFamily.UtilitarianSmall)
-			//{
-			//	var textTemplate = new CLKComplicationTemplateUtilitarianSmallFlat();
-			//	textTemplate.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
-			//	template = textTemplate;
+            CLKComplicationTemplate template = null;
 
-			//}
-			//else if (complication.Family == CLKComplicationFamily.CircularSmall)
-			//{
-			//	var textTemplate = new CLKComplicationTemplateCircularSmallRingText();
-			//	textTemplate.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
-			//	template = textTemplate;
-			//}
-			//else
-			//{
-			//	Console.WriteLine("GetPlaceholderTemplate: Complication not supported (" + complication + ")");
-			//}
+            if (complication.Family == CLKComplicationFamily.ModularSmall)
+            {
+                var textTemplate = new CLKComplicationTemplateModularSmallSimpleText();
+                textTemplate.TextProvider = CLKSimpleTextProvider.FromText("OWCE");
+                template = textTemplate;
+            }
+            else if (complication.Family == CLKComplicationFamily.GraphicCircular)
+            {
+                var graphicTemplate = new CLKComplicationTemplateGraphicCircularImage();
+                graphicTemplate.ImageProvider = new CLKFullColorImageProvider(UIImage.FromBundle("OWCELogo"));
+                template = graphicTemplate;
+            }
 
-			handler(template);
-		}
+            handler(template);
+        }
 
-		public override void GetSupportedTimeTravelDirections(CLKComplication complication, Action<CLKComplicationTimeTravelDirections> handler)
+        public override void GetSupportedTimeTravelDirections(CLKComplication complication, Action<CLKComplicationTimeTravelDirections> handler)
 		{
 			// Retrieves the time travel directions supported by your complication
 			Console.WriteLine("GetSupportedTimeTravelDirections");
