f5fbf1aaf8090fd74f54ceb857c25a9514efe25b
koa28 <86817367+koa28@users.noreply.github.com>
2021-08-31 22:05:50 -0700

    Removed a couple of unused imports

--- OWCE/OWCE.iOS/DependencyImplementations/Watch.cs
+++ OWCE/OWCE.iOS/DependencyImplementations/Watch.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using OWCE.Converters;
+﻿using System.Collections.Generic;
 using OWCE.DependencyInterfaces;
 using OWCE.PropertyChangeHandlers;
 using WatchConnectivity;
