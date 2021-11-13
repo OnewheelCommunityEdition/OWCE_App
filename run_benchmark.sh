3818770b2006bfc2b281bd7ad8e1cfd5d13fbe59
Brad Moore <brad@4pi.com.au>
2021-11-14 08:35:51 +1100

    Updated benchmark script.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,71 +2,75 @@
 LOG_FILENAME="benchmark - $(date +"%Y-%m-%d %I-%M%p").log"
 LOG_FILE="$(pwd)/$LOG_FILENAME"
 
+BUILD_CONFIG=Debug
+ANDROID_BUILD_ARGS="/p:Configuration=$BUILD_CONFIG"
+IOS_BUILD_ARGS="/p:Configuration=$BUILD_CONFIG /p:Platform=iPhoneSimulator"
+
 pushd OWCE/ > /dev/null
 
     pushd OWCE/ > /dev/null
         echo "Removing bin/obj folders and restoring nugets for OWCE.csproj"
-        echo "Removing bin/obj folders and restoring nugets for OWCE.csproj\n" >> "$LOG_FILE"
+        echo "Removing bin/obj folders and restoring nugets for OWCE.csproj" >> "$LOG_FILE"
         rm -rf bin/
         rm -rf obj/
-        msbuild OWCE.csproj /p:Configuration=Release /t:restore >> "$LOG_FILE"
+        msbuild OWCE.csproj /p:Configuration=$BUILD_CONFIG /t:restore >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
         fi
-        echo "\n\n" >> "$LOG_FILE"
+        echo "" >> "$LOG_FILE"
     popd > /dev/null
 
     pushd OWCE.Android/ > /dev/null
         echo "Removing bin/obj folders and restoring nugets for OWCE.Android.csproj"
-        echo "Removing bin/obj folders and restoring nugets for OWCE.Android.csproj\n" >> "$LOG_FILE"
+        echo "Removing bin/obj folders and restoring nugets for OWCE.Android.csproj" >> "$LOG_FILE"
         rm -rf bin/
         rm -rf obj/
-        msbuild OWCE.Android.csproj /p:Configuration=Release /t:restore >> "$LOG_FILE"
+        msbuild OWCE.Android.csproj /p:Configuration=$BUILD_CONFIG /t:restore >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
         fi
-        echo "\n\n" >> "$LOG_FILE"
+        echo "" >> "$LOG_FILE"
     popd > /dev/null
 
     pushd OWCE.iOS/ > /dev/null
         echo "Removing bin/obj folders and restoring nugets for OWCE.iOS.csproj"
-        echo "Removing bin/obj folders and restoring nugets for OWCE.iOS.csproj\n" >> "$LOG_FILE"
+        echo "Removing bin/obj folders and restoring nugets for OWCE.iOS.csproj" >> "$LOG_FILE"
         rm -rf bin/
         rm -rf obj/
-        msbuild OWCE.iOS.csproj /p:Configuration=Release /t:restore >> "$LOG_FILE"
+        msbuild OWCE.iOS.csproj /p:Configuration=$BUILD_CONFIG /t:restore >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
         fi
-        echo "\n\n" >> "$LOG_FILE"
+        echo "" >> "$LOG_FILE"
     popd > /dev/null
 
     pushd OWCE.WatchOS/OWCE.WatchOS.WatchOSApp/ > /dev/null
         echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSApp.csproj"
-        echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSApp.csproj\n" >> "$LOG_FILE"
+        echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSApp.csproj" >> "$LOG_FILE"
         rm -rf bin/
         rm -rf obj/
-        msbuild OWCE.WatchOS.WatchOSApp.csproj /p:Configuration=Release /t:restore >> "$LOG_FILE"
+        msbuild OWCE.WatchOS.WatchOSApp.csproj /p:Configuration=$BUILD_CONFIG /t:restore >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
         fi
-        echo "\n\n" >> "$LOG_FILE"
+        echo "" >> "$LOG_FILE"
     popd > /dev/null
 
     pushd OWCE.WatchOS/OWCE.WatchOS.WatchOSExtension/ > /dev/null
         echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSExtension.csproj"
-        echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSExtension.csproj\n" >> "$LOG_FILE"
+        echo "Removing bin/obj folders and restoring nugets for OWCE.WatchOS.WatchOSExtension.csproj" >> "$LOG_FILE"
         rm -rf bin/
         rm -rf obj/
-        msbuild OWCE.WatchOS.WatchOSExtension.csproj /p:Configuration=Release /t:restore >> "$LOG_FILE"
+        msbuild OWCE.WatchOS.WatchOSExtension.csproj /p:Configuration=$BUILD_CONFIG /t:restore >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
         fi
-        echo "\n\n" >> "$LOG_FILE"
+        echo "" >> "$LOG_FILE"
     popd > /dev/null
 
 
@@ -74,7 +78,7 @@ pushd OWCE/ > /dev/null
         echo "Building OWCE.Android";
 
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.Android.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.Android.csproj $ANDROID_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -85,7 +89,7 @@ pushd OWCE/ > /dev/null
         echo "Modifying OWCE.Android and building again."
         echo "//modification" >> MainActivity.cs
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.Android.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.Android.csproj $ANDROID_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -96,7 +100,7 @@ pushd OWCE/ > /dev/null
         echo "Modifying forms and building again."
         echo "//modification" >> ../OWCE/App.xaml.cs
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.Android.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.Android.csproj $ANDROID_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -109,7 +113,7 @@ pushd OWCE/ > /dev/null
         echo "Building OWCE.iOS";
 
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.iOS.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.iOS.csproj $IOS_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -120,7 +124,7 @@ pushd OWCE/ > /dev/null
         echo "Modifying OWCE.iOS and building again."
         echo "//modification" >> AppDelegate.cs
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.iOS.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.iOS.csproj $IOS_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -131,7 +135,7 @@ pushd OWCE/ > /dev/null
         echo "Modifying forms and building again."
         echo "//modification" >> ../OWCE/App.xaml.cs
         BUILD_START_TIME=$(date +%s)
-        msbuild OWCE.iOS.csproj /p:Configuration=Release >> "$LOG_FILE"
+        msbuild OWCE.iOS.csproj $IOS_BUILD_ARGS >> "$LOG_FILE"
         if [ $? -ne 0 ]; then
             echo "Unable to complete action. Please check your log file ($LOG_FILENAME) for more information."
             exit
@@ -143,24 +147,31 @@ pushd OWCE/ > /dev/null
   
 popd > /dev/null
 
+
+TOTAL_BUILD_TIME=$(($ANDROID_BUILD_TIME+$ANDROID_REBUILD_TIME+$ANDROID_FORMS_REBUILD_TIME+$IOS_BUILD_TIME+$IOS_REBUILD_TIME+$IOS_FORMS_REBUILD_TIME))
+
 echo "" >> "$LOG_FILE"
 echo "" >> "$LOG_FILE"
-
-echo "Android build time: $ANDROID_BUILD_TIME" >> "$LOG_FILE"
-echo "Android re-build time: $ANDROID_REBUILD_TIME" >> "$LOG_FILE"
-echo "Android forms re-build time: $ANDROID_FORMS_REBUILD_TIME" >> "$LOG_FILE"
-
-echo "iOS build time: $IOS_BUILD_TIME" >> "$LOG_FILE"
-echo "iOS re-build time: $IOS_REBUILD_TIME" >> "$LOG_FILE"
-echo "iOS forms re-build time: $IOS_FORMS_REBUILD_TIME" >> "$LOG_FILE"
-
+echo "Android build time: $ANDROID_BUILD_TIME seconds" >> "$LOG_FILE"
+echo "Android re-build time: $ANDROID_REBUILD_TIME seconds" >> "$LOG_FILE"
+echo "Android forms re-build time: $ANDROID_FORMS_REBUILD_TIME seconds" >> "$LOG_FILE"
+echo "" >> "$LOG_FILE"
+echo "iOS build time: $IOS_BUILD_TIME seconds" >> "$LOG_FILE"
+echo "iOS re-build time: $IOS_REBUILD_TIME seconds" >> "$LOG_FILE"
+echo "iOS forms re-build time: $IOS_FORMS_REBUILD_TIME seconds" >> "$LOG_FILE"
+echo "" >> "$LOG_FILE"
+echo "Total build time: $TOTAL_BUILD_TIME seconds" >> "$LOG_FILE"
+echo "" >> "$LOG_FILE"
 
 echo ""
 echo ""
-echo "Android build time: $ANDROID_BUILD_TIME"
-echo "Android re-build time: $ANDROID_REBUILD_TIME"
-echo "Android forms re-build time: $ANDROID_FORMS_REBUILD_TIME"
-
-echo "iOS build time: $IOS_BUILD_TIME"
-echo "iOS re-build time: $IOS_REBUILD_TIME"
-echo "iOS forms re-build time: $IOS_FORMS_REBUILD_TIME"
+echo "Android build time: $ANDROID_BUILD_TIME seconds"
+echo "Android re-build time: $ANDROID_REBUILD_TIME seconds"
+echo "Android forms re-build time: $ANDROID_FORMS_REBUILD_TIME seconds"
+echo ""
+echo "iOS build time: $IOS_BUILD_TIME seconds"
+echo "iOS re-build time: $IOS_REBUILD_TIME seconds"
+echo "iOS forms re-build time: $IOS_FORMS_REBUILD_TIME seconds"
+echo ""
+echo "Total build time: $TOTAL_BUILD_TIME seconds"
+echo ""
