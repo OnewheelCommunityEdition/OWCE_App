ae2523339f4bde99bc4b9e8eaa728503144c2b6e
Brad <brad@4pi.com.au>
2022-07-03 17:57:23 +1000

    Added initial wearOS app.

---
+++ OWCE/OWCE.WearOS/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Activity;
+
+namespace OWCE.WearOS
+{
+    [Activity(Label = "OWCE", MainLauncher = true)]
+    public class MainActivity : WearableActivity
+    {
+        TextView textView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+
+            textView = FindViewById<TextView>(Resource.Id.text);
+            SetAmbientEnabled();
+        }
+    }
+}
+
+
+
