dd93f39217c5bd77d8e80b79cdd35ff069f6f17a
Brad <brad@4pi.com.au>
2022-07-03 20:01:18 +1000

    More getting wearOS to a functional state.

--- OWCE/OWCE.Android/DependencyImplementations/Watch.cs
+++ OWCE/OWCE.Android/DependencyImplementations/Watch.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Android.Gms.Wearable;
 using OWCE.DependencyInterfaces;
 using Xamarin.Forms;
 
@@ -6,21 +10,140 @@ using Xamarin.Forms;
 
 namespace OWCE.Droid.DependencyImplementations
 {
-    public class Watch : IWatch
+    public class Watch : Java.Lang.Object,
+        IWatch,
+        MessageClient.IOnMessageReceivedListener,
+        CapabilityClient.IOnCapabilityChangedListener
     {
+        ICollection<INode> connectedDevices;
+        MessageClient messageClient;
+        Dictionary<WatchMessage, object> messageCache = new Dictionary<WatchMessage, object>();
+
         public void SendWatchMessages(Dictionary<WatchMessage, object> messages)
         {
+            foreach (var message in messages)
+            {
+                messageCache[message.Key] = message.Value;
+            }
+
+            // If there are no nodes we have nothing to send.
+            if (connectedDevices.Any() == false)
+            {
+                return;
+            }
+
             // Implement when we have Android Watch support
+            //Debugger.Break();
+
+            if (messages.ContainsKey(WatchMessage.Voltage))
+            {
+                if (messages[WatchMessage.Voltage] is float value)
+                {
+                    var data = BitConverter.GetBytes(value);
+                    foreach (var connectedDevice in connectedDevices)
+                    {
+                        //System.Diagnostics.Debug.WriteLine($"Phone: Attempting to send to {node.DisplayName}");
+                        messageClient.SendMessage(connectedDevice.Id, "/voltage", data);
+                    }
+                }
+
+                // float
+                //Debugger.Break();
+            }
+            else if (messages.ContainsKey(WatchMessage.Speed))
+            {
+                if (messages[WatchMessage.Speed] is int value)
+                {
+                    var data = BitConverter.GetBytes(value);
+                    foreach (var connectedDevice in connectedDevices)
+                    {
+                        //System.Diagnostics.Debug.WriteLine($"Phone: Attempting to send to {node.DisplayName}");
+                        messageClient.SendMessage(connectedDevice.Id, "/speed", data);
+                    }
+                }
+
+                // int
+                // Debugger.Break();
+            }
+            else if (messages.ContainsKey(WatchMessage.BatteryPercent))
+            {
+                if (messages[WatchMessage.BatteryPercent] is int value)
+                {
+                    var data = BitConverter.GetBytes(value);
+                    foreach (var connectedDevice in connectedDevices)
+                    {
+                        //System.Diagnostics.Debug.WriteLine($"Phone: Attempting to send to {node.DisplayName}");
+                        messageClient.SendMessage(connectedDevice.Id, "/battery_percent", data);
+                    }
+                }
+
+                // int
+                //Debugger.Break();
+            }
+            else if (messages.ContainsKey(WatchMessage.Distance))
+            {
+                if (messages[WatchMessage.Distance] is string value)
+                {
+                    var data = System.Text.Encoding.ASCII.GetBytes(value);
+                    foreach (var connectedDevice in connectedDevices)
+                    {
+                        //System.Diagnostics.Debug.WriteLine($"Phone: Attempting to send to {node.DisplayName}");
+                        messageClient.SendMessage(connectedDevice.Id, "/distance", data);
+                    }
+                }
+
+                // string
+                //Debugger.Break();
+            }
+            else if (messages.ContainsKey(WatchMessage.SpeedUnitsLabel))
+            {
+                Debugger.Break();
+            }
+            else if (messages.ContainsKey(WatchMessage.Awake))
+            {
+                Debugger.Break();
+            }
         }
 
         void IWatch.ListenForWatchMessages(OWBoard board)
         {
             // Implement when we have Android Watch support
+            //Debugger.Break();
+            SetupCapability();
         }
 
         void IWatch.StopListeningForWatchMessages()
         {
             // Implement when we have Android Watch support
+            Debugger.Break();
         }
+
+        async void SetupCapability()
+        {
+            messageClient = WearableClass.GetMessageClient(Xamarin.Essentials.Platform.CurrentActivity);
+            messageClient.AddListener(this);
+
+            var capabilityInfo = await WearableClass.GetCapabilityClient(Xamarin.Essentials.Platform.CurrentActivity).GetCapabilityAsync("owce", CapabilityClient.FilterReachable);
+            connectedDevices = capabilityInfo.Nodes;
+
+            WearableClass.GetCapabilityClient(Xamarin.Essentials.Platform.CurrentActivity).AddListener(this, "owce");
+        }
+
+
+        #region CapabilityClient.IOnCapabilityChangedListener 
+        public void OnCapabilityChanged(ICapabilityInfo capabilityInfo)
+        {
+            connectedDevices = capabilityInfo.Nodes;
+
+            // TODO: Send messageCache
+        }
+        #endregion
+
+        #region MessageClient.IOnMessageReceivedListener
+        public void OnMessageReceived(IMessageEvent messageEvent)
+        {
+            System.Diagnostics.Debug.WriteLine("Phone: OnMessageReceived");
+        }
+        #endregion
     }
 }
--- OWCE/OWCE.WearOS/MainActivity.cs
+++ OWCE/OWCE.WearOS/MainActivity.cs
@@ -4,22 +4,116 @@ using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Support.Wearable.Activity;
+using Android.Gms.Wearable;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace OWCE.WearOS
 {
     [Activity(Label = "OWCE", MainLauncher = true)]
-    public class MainActivity : WearableActivity
+    public class MainActivity : WearableActivity,
+        MessageClient.IOnMessageReceivedListener,
+        CapabilityClient.IOnCapabilityChangedListener
     {
-        TextView textView;
+        //TextView textView;
 
+        ICollection<INode> nodes;
+
+        TextView notConnectedWarningTextView;
+        TextView speedTextView;
+        TextView batteryPercentTextView;
+        TextView voltageTextView;
+        TextView distanceTextView;
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.activity_main);
 
-            textView = FindViewById<TextView>(Resource.Id.text);
+            notConnectedWarningTextView = FindViewById<TextView>(Resource.Id.not_connected_warning);
+            speedTextView = FindViewById<TextView>(Resource.Id.speed);
+            batteryPercentTextView = FindViewById<TextView>(Resource.Id.battery_percent);
+            voltageTextView = FindViewById<TextView>(Resource.Id.voltage);
+            distanceTextView = FindViewById<TextView>(Resource.Id.distance);
+            
+
+
             SetAmbientEnabled();
+
+            SetupCapability();
         }
+
+        void SetupCapability()
+        {
+            WearableClass.GetMessageClient(this).AddListener(this);
+            //await WearableClass.GetMessageClient(this).AddListenerAsync(this);
+
+            //var capabilityInfo = await WearableClass.GetCapabilityClient(this).GetCapabilityAsync("owce", CapabilityClient.FilterReachable);
+            //nodes = capabilityInfo.Nodes;
+
+            WearableClass.GetCapabilityClient(this).AddListener(this, "owce");
+        }
+
+
+        #region CapabilityClient.IOnCapabilityChangedListener
+        public void OnCapabilityChanged(ICapabilityInfo capabilityInfo)
+        {
+            nodes = capabilityInfo.Nodes;
+            Debugger.Break();
+        }
+        #endregion
+
+        #region MessageClient.IOnMessageReceivedListener
+        public void OnMessageReceived(IMessageEvent messageEvent)
+        {
+            if (notConnectedWarningTextView.Visibility == Android.Views.ViewStates.Visible)
+            {
+                notConnectedWarningTextView.Visibility = Android.Views.ViewStates.Gone;
+                speedTextView.Visibility = Android.Views.ViewStates.Visible;
+                batteryPercentTextView.Visibility = Android.Views.ViewStates.Visible;
+                voltageTextView.Visibility = Android.Views.ViewStates.Visible;
+                distanceTextView.Visibility = Android.Views.ViewStates.Visible;
+            }
+
+
+            var data = messageEvent.GetData();
+
+            if (messageEvent.Path == "/speed")
+            {
+                var value = BitConverter.ToInt32(data);
+                speedTextView.Text = $"{value}";
+                //Debugger.Break();
+            }
+            else if (messageEvent.Path == "/voltage")
+            {
+                var value = BitConverter.ToSingle(data);
+                voltageTextView.Text = $"{value:2}V";
+                //Debugger.Break();
+            }
+            else if (messageEvent.Path == "/battery_percent")
+            {
+                var value = BitConverter.ToInt32(data);
+                batteryPercentTextView.Text = $"{value}%";
+                //Debugger.Break();
+            }
+            else if (messageEvent.Path == "/distance")
+            {
+                var value = System.Text.Encoding.ASCII.GetString(data);
+                distanceTextView.Text = value;
+                //Debugger.Break();
+
+            }
+            else if (messageEvent.Path == "/awake")
+            {
+
+            }
+            else if (messageEvent.Path == "/speed_units_label")
+            {
+
+            }
+        }
+        #endregion
     }
 }
 
